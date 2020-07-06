8d68cc2944a99b4da00350df7c40fe644f913b1d
udaraJ08 <67888873+udaraJ08@users.noreply.github.com>
2020-07-06 20:40:26 +0530

    inputs added
    Added some inputs

--- phoenix.java
+++ phoenix.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 class Example{
   
     public static void main(String[] args){
     
+        Scanner input = new Scanner(System.in);
+      
         System.out.println("Hello world this is my first OFFICIAL git hub commit attempt !!!");
+      
+        System.out.print("Enter number );
+        int num1 = input.nextInt();
+        
+        System.out.print("Enter number 2: ");
+        int num2 = input.nextInt();
+                         
+        System.out.println("Sum is " +(num1+num2));
     }
 }
